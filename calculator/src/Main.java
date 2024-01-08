016d4b2a2a59fc88789a60ffebf0ef22981d612c
Chris M <christopher.p.mohr@gmail.com>
2024-01-08 13:16:42 -0800

    Square root and Power of is all complete.

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
     public static double addition(double numberOne, double numberTwo) {
@@ -19,6 +20,28 @@ public class Main {
     public static double divide(double numberOne, double numberTwo) {
 
         return numberOne / numberTwo;
+    }
+
+    public static double squareRootMethod(Scanner scnr) {
+
+        int numberToSquare;
+        System.out.println("Enter the number to be squared: ");
+        numberToSquare = scnr.nextInt();
+
+        return Math.sqrt(numberToSquare);
+    }
+
+    public static double powerOf(Scanner scnr) {
+        double baseNumber;
+        int ofPower;
+
+        System.out.println("Enter the base number: ");
+        baseNumber = scnr.nextInt();
+
+        System.out.println("Enter the power of the number: ");
+        ofPower = scnr.nextInt();
+
+        return Math.pow(baseNumber, ofPower);
     }
 
     public static double calculations(char equation, double numberOne, double numberTwo ) {
@@ -51,8 +74,18 @@ public class Main {
         needComplexNum = scnr.nextLine().toLowerCase();
 
         if (needComplexNum.equals("yes")) {
-            System.out.println("System is not ready for this yet.");
+            String complexProcess;
+            System.out.println("Currently square root and \"power of\" is available (sqrt & powr): ");
+            complexProcess = scnr.nextLine();
 
+            switch (complexProcess) {
+                case "sqrt":
+                    System.out.println("The square root is: " + squareRootMethod(scnr));
+                    break;
+                case "powr":
+                    System.out.println("Total is: " + powerOf(scnr));
+                    break;
+                }
         } else {
 
             boolean needMore = true;
